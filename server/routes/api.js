fd7ad2e0b33b18a2330452f8d94aa17800550b81
Geebergy <obeingilbert3884@gmail.com>
2024-03-24 02:12:46 +0100

    Update api.js

--- server/routes/api.js
+++ server/routes/api.js
@@ -85,7 +85,7 @@ router.post("/addUser", async (request, response) => {
 });
 // reset and set leadderboard
 // Schedule task to run at 00:00 on Monday (start of the week)
-cron.schedule('0 0 * * 1', async () => {
+cron.schedule('0 0 * * 0', async () => {
   try {
       // Reset weeklyEarnings and adsClicked for all users
       await User.updateMany({}, { $set: { weeklyEarnings: 0, adsClicked: 0 } });
