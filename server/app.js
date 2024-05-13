0523e7ab7ddd887964bcce86ec93af27ab2db6a0
Geebergy <obeingilbert3884@gmail.com>
2024-05-13 20:42:12 +0100

    Cors policy

--- server/app.js
+++ server/app.js
@@ -5,6 +5,11 @@ const apiRouter = require('./routes/api');
 
 const app = express();
 app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // Allow access from any origin
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.json());
 const PORT = process.env.PORT || 3001;
 
