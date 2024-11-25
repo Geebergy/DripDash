516b38b6a19a33221dd29e418324861da3988af4
Geebergy <obeingilbert3884@gmail.com>
2024-11-25 13:19:27 +0100

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,8 +1,3 @@
-const express = require('express');
-const cors = require('cors');
-const apiRouter = require('./routes/api');
-const { spawn } = require('child_process'); // Import child_process module
-
 const app = express();
 app.use(cors());
 app.use(function(req, res, next) {
@@ -13,23 +8,7 @@ app.use(function(req, res, next) {
 app.use(express.json());
 const PORT = process.env.PORT || 3001;
 
-app.use('/api', apiRouter);
-
-// Run bot.js as a separate process
-const botProcess = spawn('node', ['./bot.js']); // Ensure the path is correct
-
-// Log bot.js output to console
-botProcess.stdout.on('data', (data) => {
-  console.log(`Bot output: ${data}`);
-});
-
-botProcess.stderr.on('data', (data) => {
-  console.error(`Bot error: ${data}`);
-});
-
-botProcess.on('exit', (code) => {
-  console.log(`Bot process exited with code ${code}`);
-});
+app.use('/api', apiRouter); 
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
